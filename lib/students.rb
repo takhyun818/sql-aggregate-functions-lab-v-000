6f3de688e85f390151cea490df72fd809ad9c3d7
Tak H Kim <takhyun818@gmail.com>
2019-01-17 22:52:51 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  
+
 end
 
 def lowest_student_gpa
@@ -15,5 +15,5 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
-  
+
 end
