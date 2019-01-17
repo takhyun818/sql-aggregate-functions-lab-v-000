b1e43a2631adbd342820d0ea98f17fdbcc7a8623
Tak H Kim <takhyun818@gmail.com>
2019-01-17 22:52:44 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,6 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
+  
 end
 
 def lowest_student_gpa
@@ -10,7 +11,9 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
+  SELECT SUM(tardies) from students
 end
 
 def average_gpa_for_9th_grade
+  
 end
